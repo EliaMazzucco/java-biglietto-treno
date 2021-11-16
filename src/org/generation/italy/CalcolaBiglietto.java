802e818717f9ee1a70c09944070763f49ebe15e3
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-16 14:58:23 +0100

    lavoro terminato

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -3,9 +3,53 @@ package org.generation.italy;
 import java.util.Scanner;
 
 public class CalcolaBiglietto {
+	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		//Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero
+		double kmDaPercorrere;
+		int anni;
+		
+		System.out.print("Inserisci il numero di chilometri di percorrere: ");
+		kmDaPercorrere = scanner.nextDouble();	
+		System.out.print("Inserisci la tua età: ");
+		anni = scanner.nextInt();		
+		
+		//debug
+		System.out.println("I tuoi chilometri: " + kmDaPercorrere);
+		System.out.println("La tua età: " + anni);
+		
+		//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+		
+		
+		//-il prezzo del biglietto è definito in base ai km (0.21 € al km)
+		double prezzoKm = 0.21;
+		
+		double prezzoBiglietto = kmDaPercorrere * prezzoKm;
+		
+		//debug
+		System.out.println("Il prezzo del tuo biglietto è: " + prezzoBiglietto);
+		
+		//-va applicato uno sconto del 20% per i minorenni
+		
+		double scontoMinorenni = (prezzoBiglietto *20) / 100;
+		
+		if(anni < 18) {
+			System.out.println("Il prezzo del tuo biglietto scontato è: " + (prezzoBiglietto - scontoMinorenni));			
+		}
+		
+		//-va applicato uno sconto del 40% per gli over 65
+		
+		double scontoOver = (prezzoBiglietto *20) / 100;
+		
+		if(anni > 65) {
+			System.out.println("Il prezzo del tuo biglietto scontato è: " + (prezzoBiglietto - scontoOver));			
+		}
+		
+		
+		
+		
 		
 		scanner.close();
 	}
