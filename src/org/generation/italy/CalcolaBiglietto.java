1b9cbe594e0ac3c454576efc957b19ad706e1e4b
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-16 14:19:29 +0100

    primo commit

---
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -0,0 +1,13 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		scanner.close();
+	}
+
+}
